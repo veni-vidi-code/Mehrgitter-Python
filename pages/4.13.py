8fd82cb72b8c0e583cbf15fc890e831b89bb478a
veni-vidi-code <tom.mucke@web.de>
2022-10-21 17:29:19 +0200

    Adds callback to remove not defined options

--- pages/4.13.py
+++ pages/4.13.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc, callback, Input, Output, State
 from implementations import mehrgitterhelper
 import plotly.graph_objects as go
 
@@ -21,16 +21,22 @@ layout = html.Div(children=[
 
 
 @callback(Output('fourier-modes', 'figure'), Input('l', 'value'), Input('j', 'value'))
-def change_gitter(l, j):
-    y = mehrgitterhelper.fourier_mode(l, j)
+def change_gitter(stufenindex_l, j):
+    y = mehrgitterhelper.fourier_mode(stufenindex_l, j)
     linear_gitter = LINEAR_GITTERHIERACHIE
     trivial_gitter = TRIVIAL_GITTERHIERACHIE
-    x = linear_gitter.get_gitterfolge(l)
-    x2 = linear_gitter.get_gitterfolge(l - 1)
-    y_linear = linear_gitter.get_restriktionsmatrix(l) @ y
-    y_trivial = trivial_gitter.get_restriktionsmatrix(l) @ y
+    x = linear_gitter.get_gitterfolge(stufenindex_l)
+    x2 = linear_gitter.get_gitterfolge(stufenindex_l - 1)
+    y_linear = linear_gitter.get_restriktionsmatrix(stufenindex_l) @ y
+    y_trivial = trivial_gitter.get_restriktionsmatrix(stufenindex_l) @ y
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers', name='original'))
     fig.add_trace(go.Scatter(x=x2, y=y_trivial, mode='lines+markers', name='trivial'))
     fig.add_trace(go.Scatter(x=x2, y=y_linear, mode='lines+markers', name='linear'))
     return fig
+
+
+@callback(Output('j', 'max'), Output('j', 'value'), Input('l', 'value'), State('j', 'value'))
+def change_j_max(stufenindex_l, j):
+    n = (2 ** (stufenindex_l + 1)) - 1
+    return n, min(n, j)
