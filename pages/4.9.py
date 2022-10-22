0ebae33c5529f9f870b19f4bd8cdc448d1658da5
veni-vivi-code <tom.mucke@web.de>
2022-10-22 17:31:28 +0200

    Makes it possible to choose other start vectors for 4.9

--- pages/4.9.py
+++ pages/4.9.py
@@ -2,7 +2,7 @@ from implementations.dirichlect import get_jacobi_generator, N_l
 
 import numpy as np
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc, callback, Input, Output, State, ALL
 
 import plotly.graph_objects as go
 
@@ -10,7 +10,23 @@ from pages.cache import cache
 
 dash.register_page(__name__, name="Fehler Dämpfung Jacobi")
 
-example_startfault = np.array([0.75, 0.2, 0.6, 0.45, 0.9, 0.6, 0.8, 0.85, 0.55, 0.7, 0.9, 0.5, 0.6, 0.3, 0.2])
+max_l = 4
+
+example_startfault = np.array([0.75, 0.2, 0.6, 0.45, 0.9, 0.6, 0.8,
+                               0.85, 0.55, 0.7, 0.9, 0.5, 0.6, 0.3, 0.2])  # This is the one in the book
+startfaults = [(np.random.rand(N_l(l)) if l != 3 else example_startfault) for l in range(1, max_l + 1)]
+
+default_array_div = []
+for l in range(1, max_l + 1):
+    x = []
+    vec = startfaults[l - 1]
+    for i in range(len(vec)):
+        x.append(dcc.Input(placeholder=f"Start Fehler {i}", type="number", value=vec[i],
+                           id={'type': 'startfault-4-9',
+                               'index': i},
+                           inputMode="numeric"))
+        x.append(html.Br())
+    default_array_div.append(x)
 
 layout = html.Div(children=[
     html.H1(children='Fehler relaxiertes Jacobi Verfahren'),
@@ -23,15 +39,19 @@ layout = html.Div(children=[
             id="snapping-4-9"
         ),
         "Gitter (l): ",
-        dcc.Slider(1, 4, 1, value=3, id="l-4-9"),
+        dcc.Slider(1, max_l, 1, value=3, id="l-4-9"),
         "Dämpfung (w): ",
         dcc.Slider(0, 0.5, step=1e-6, marks={
             0.5: '1/2',
             1 / 3: '1/3',
             1 / 4: '1/4',
             1 / 8: '1/8'
-        }, value=1/4, id="w-4-9", tooltip={"placement": "bottom"}),
-
+        }, value=1 / 4, id="w-4-9", tooltip={"placement": "bottom"}),
+        html.Details(children=[
+            html.Summary(children="Start Fehler"),
+            html.Div(id="fault_vector_div-4-9"),
+            html.Button(id='submit-button-4-9', children='Update', n_clicks=0)
+        ]),
     ]),
     html.Br(),
     dcc.Graph(id='iter-graph-4-9'),
@@ -56,10 +76,17 @@ def _generate_fig(stufenindex_l, w, start: np.ndarray):
     return fig
 
 
-@callback(Output('iter-graph-4-9', 'figure'), Input('l-4-9', 'value'),
-          Input('w-4-9', 'value'))
-def add_traces(stufenindex_l, w):
-    return _generate_fig(stufenindex_l, w, example_startfault)
+@callback(Output('iter-graph-4-9', 'figure'), Output('fault_vector_div-4-9', 'children'),
+          Input('l-4-9', 'value'),
+          Input('w-4-9', 'value'),
+          State({'type': 'startfault-4-9', 'index': ALL}, 'value'),
+          Input('submit-button-4-9', 'n_clicks'))
+def add_traces(stufenindex_l, w, vector, n_clicks):
+    if dash.ctx.triggered_id is None or dash.ctx.triggered_id.startswith("l-4-9"):
+        vector = startfaults[stufenindex_l - 1]
+        return _generate_fig(stufenindex_l, w, np.array(vector)), default_array_div[stufenindex_l - 1]
+    else:
+        return _generate_fig(stufenindex_l, w, np.array(vector)), dash.no_update
 
 
 @callback(Output('w-4-9', 'step'), Input('snapping-4-9', 'value'))
