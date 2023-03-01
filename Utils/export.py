11f4b804d7102fe19e5abc467b1bc05a49b22adc
veni-vivi-code <tom.mucke@web.de>
2023-03-01 11:58:02 +0100

    Removes margins

--- Utils/export.py
+++ Utils/export.py
@@ -4,6 +4,7 @@ import time
 import dash
 from plotly.graph_objects import Figure
 import plotly.io as pio
+from plotly.graph_objs import Layout
 
 asset_image_path = 'assets/images'
 
@@ -17,7 +18,11 @@ pio.kaleido.scope.default_scale = 1
 
 def save_image(figure, figname, **kwargs):
     if not isinstance(figure, Figure):
+        layout = dict(
+            margin=dict(l=0, r=0, t=0, b=0),
+        )
         figure = Figure(figure)
+        figure.update_layout(layout)
     folder = os.path.join(asset_image_path, figname)
     if not os.path.exists(folder):
         os.mkdir(folder)
